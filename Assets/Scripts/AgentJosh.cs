302900aa9fcb02fa9cc8f47c02fb67a9ac25303d
Reece <reecebellchambers98@gmail.com>
2024-01-28 13:07:31 +0000

    JoshAIPathfinding

---
+++ Assets/Scripts/AgentJosh.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentJosh : AgentAi
+{
+    public override void Walking()
+    {
+        if (waypoints.Count == 0.0f) { return; }
+        //if (distracted) { return; }
+
+        distanceToWaypoint = Vector3.Distance(waypoints[currentWaypointIndex].position, transform.position);
+        if (distanceToWaypoint <= 1.1f)
+        {
+            StartCoroutine(StandingStill());
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Count;
+        }
+
+        navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+    }
+    private IEnumerator StandingStill()
+    {
+        navMeshAgent.speed = 0.0f;
+        animator.SetBool("IsIdle", true);
+        animator.SetBool("IsWalking", false);
+        yield return new WaitForSeconds(5.0f);
+        animator.SetBool("IsIdle", false);
+        animator.SetBool("IsWalking", true);
+        navMeshAgent.speed = 2.0f;
+    }
+}
